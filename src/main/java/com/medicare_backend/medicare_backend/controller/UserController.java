5bd8dc780c3535cea838e2157f734a3bcde62abf
sawid <zasawidza@gmail.com>
2022-09-17 17:47:38 +0700

    update read data in database

--- src/main/java/com/medicare_backend/medicare_backend/controller/UserController.java
+++ src/main/java/com/medicare_backend/medicare_backend/controller/UserController.java
@@ -4,11 +4,14 @@ import com.medicare_backend.medicare_backend.service.UserService;
 import com.medicare_backend.medicare_backend.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +25,13 @@ public class UserController {
         return userService.getListUser();
     }
 
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserByOneUser(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
+    }
+
     @PostMapping("/users")
-    public void newUser(@RequestBody User user) {
+    public void registerNewUser(@RequestBody User user) {
         userService.registerUser(user);
     }
 
--- src/main/java/com/medicare_backend/medicare_backend/service/UserService.java
+++ src/main/java/com/medicare_backend/medicare_backend/service/UserService.java
@@ -2,9 +2,12 @@ package com.medicare_backend.medicare_backend.service;
 
 import com.medicare_backend.medicare_backend.entity.User;
 import com.medicare_backend.medicare_backend.repository.UserRepository;
+import com.medicare_backend.medicare_backend.service.AuthenticationService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +18,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private AuthenticationService authservice;
+
+
     public void registerUser(User user) {
         try {
+            byte[] hash = authservice.getEncryptedPassword(user.getPasswordId(), "salt".getBytes());
+            // System.out.println(Arrays.toString(hash));
+            // System.out.println(authservice.bytesToHex(hash));
+            user.setPasswordId(authservice.bytesToHex(hash));
+            byte[] passwordToByte = authservice.hexToByte("C81BDC96AFBB8AD447C176B2CD1DBFE1568C11EE8B6205DE29B27F336B3AE2DE8B122880A4FFB1C3A85FE9F391EE762DD0A990A73F10DAF44EC693A33EE8B7C3");
+            System.out.println(authservice.authenticate("FolkSawit@gmail.com", passwordToByte, "salt".getBytes()));
+            System.out.println(authservice.authenticate("FolkSawit@gmail.com", passwordToByte, "salty".getBytes()));
             userRepository.save(user);
         } catch (Exception e) {
             System.out.println(e);
@@ -31,6 +44,10 @@ public class UserService {
             System.out.println(e);
         }
         return user;
+    }
+
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
     }
 
 }
