a43be6f8a09f9ae6f0fc808c1f9f7a2bcd064bd8
Notdakon Chantarat <69079640+NtdChnn@users.noreply.github.com>
2022-11-07 12:11:54 +0700

    edit api update schedule

--- src/main/java/com/medicare_backend/medicare_backend/controller/ScheduleController.java
+++ src/main/java/com/medicare_backend/medicare_backend/controller/ScheduleController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 import com.medicare_backend.medicare_backend.schema.entity.Schedule;
 import com.medicare_backend.medicare_backend.service.ScheduleService;
+import com.medicare_backend.medicare_backend.service.AppointmentService;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,10 +18,13 @@ import java.util.Optional;
 public class ScheduleController {
     
     private ScheduleService scheduleService;
+    private AppointmentService appointmentService;
 
     @Autowired
-    public ScheduleController(ScheduleService scheduleService){
+    public ScheduleController(  ScheduleService scheduleService,
+                                AppointmentService appointmentService){
         this.scheduleService = scheduleService;
+        this.appointmentService = appointmentService;
     }
 
     @GetMapping(path = "/schedules")
@@ -49,6 +55,33 @@ public class ScheduleController {
         } else {
             return ResponseEntity.status(500).body(data);
         }
+    }
+
+    @PutMapping(path = "/schedules/update/{id}") //not finish //not check duplicate schedule yet and not update to Appoinment yet
+    public ResponseEntity<?> updateSchedule(
+                @PathVariable("id") long scheduleId,
+                @RequestParam(required = true) int scheduleCapacity, //can't update to be 0
+                @RequestParam(required = false) LocalDateTime scheduleStart,
+                @RequestParam(required = false) LocalDateTime scheduleEnd,
+                @RequestParam(required = false) LocalDate scheduleDate,
+                @RequestParam(required = false) String scheduleLocation,
+                @RequestParam(required = true) long appointmentDoctorId //can't update to be 0 
+                //if don't want to update DoctorId or Capacity sent param as 0
+                //because long and int can't be null
+                ){
+        scheduleService.updateSchedule( scheduleId,
+                                        scheduleCapacity,
+                                        scheduleStart,
+                                        scheduleEnd,
+                                        scheduleDate,
+                                        scheduleLocation);
+        appointmentService.updateApponimentFromSchedule(scheduleId,
+                                                        scheduleStart,
+                                                        scheduleEnd,
+                                                        scheduleDate,
+                                                        scheduleLocation,
+                                                        appointmentDoctorId);
+        return ResponseEntity.ok().body("Schedule with ID: " + scheduleId + " has update successfully");
     }
 
 }
