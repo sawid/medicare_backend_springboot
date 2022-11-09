1710af1577796aec116e2d781c5c6e9be5696d72
Notdakon Chantarat <69079640+NtdChnn@users.noreply.github.com>
2022-11-09 10:06:57 +0700

    add try catch

--- src/main/java/com/medicare_backend/medicare_backend/service/AppointmentService.java
+++ src/main/java/com/medicare_backend/medicare_backend/service/AppointmentService.java
@@ -2,6 +2,7 @@ package com.medicare_backend.medicare_backend.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -25,61 +26,96 @@ public class AppointmentService {
 
     //Get all Appointment
     public List<Appointment> getAppointment() {
-        return appointmentRepository.findAll();
+        List<Appointment> appointments = new ArrayList<>();
+        try {
+            appointments = appointmentRepository.findAll();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return appointments;
     }
 
     //Get Appointment by Id
     public Optional<Appointment> getAppointmentById(Long appointmentId) {
-        return appointmentRepository.findById(appointmentId);
+        Optional<Appointment> appointment = Optional.empty();
+        try {
+            appointment = appointmentRepository.findById(appointmentId);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return appointment;
     }
 
     //Create new Appointment
     public String createNewAppointment(Appointment appointment) {
+        try{
         appointmentRepository.save(appointment);
+        } catch(Exception e){
+            System.out.println(e);
+        }
         return "Create Success";
     }
 
     //Get Appointment by ScheduleId
     public List<Appointment> getAppointmentByScheduleId(long appointmentScheduleId) {
-        return appointmentRepository.findAppointmentByappointmentScheduleId(appointmentScheduleId);
+        List<Appointment> appointments = new ArrayList<>();
+        try {
+            appointments = appointmentRepository.findAppointmentByappointmentScheduleId(appointmentScheduleId);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return appointments;
     }
 
     //Update Appointment from Schedule
     @Transactional
-    public void updateApponimentFromSchedule(long scheduleId, 
+    public List<Long> updateApponimentFromSchedule(long scheduleId, 
                                             LocalDateTime appointmentTimeStart, 
                                             LocalDateTime appointmentTimeEnd,
                                             LocalDate appointmentDate, 
                                             String appointmentLocation, 
                                             long appointmentDoctorId) {
         List<Appointment> appointments = getAppointmentByScheduleId(scheduleId);
-        if(appointments == null || appointments.isEmpty()){
-            //return have no appointment with this scheduleId
-        }
+        List<Long> patientHNId = new ArrayList<>();
+        try {
+            if(appointments == null || appointments.isEmpty()){
+                return patientHNId;
+            }
         
-        for(Appointment a : appointments){
-            if(appointmentTimeStart != null && !Objects.equals(a.getAppiontmentTimeStart(), appointmentTimeStart)){
-                a.setAppiontmentTimeStart(appointmentTimeStart);
-                //add sth to list                
+            for(Appointment a : appointments){
+                if(appointmentTimeStart != null && !Objects.equals(a.getAppiontmentTimeStart(), appointmentTimeStart)){
+                    a.setAppiontmentTimeStart(appointmentTimeStart);
+                    patientHNId.add(a.getAppointmentPatientId());              
+                }
+                if(appointmentTimeEnd != null && !Objects.equals(a.getAppiontmentTimeEnd(), appointmentTimeEnd)){
+                    a.setAppiontmentTimeEnd(appointmentTimeEnd);
+                    if(!patientHNId.contains(a.getAppointmentPatientId())){
+                        patientHNId.add(a.getAppointmentPatientId());
+                    }               
+                }
+                if(appointmentDate != null && !Objects.equals(a.getAppointmentDate(), appointmentDate)){
+                    a.setAppointmentDate(appointmentDate);
+                    if(!patientHNId.contains(a.getAppointmentPatientId())){
+                        patientHNId.add(a.getAppointmentPatientId());
+                    }                 
+                }
+                if(appointmentLocation != null && appointmentLocation.length() > 0 && !Objects.equals(a.getAppiontmentLocation(), appointmentLocation)){
+                    a.setAppiontmentLocation(appointmentLocation);
+                    if(!patientHNId.contains(a.getAppointmentPatientId())){
+                        patientHNId.add(a.getAppointmentPatientId());
+                    }    
+                }
+                if(appointmentDoctorId != 0 && !Objects.equals(a.getAppointmentDoctorId(), appointmentDoctorId)){
+                    a.setAppointmentDoctorId(appointmentDoctorId);
+                    if(!patientHNId.contains(a.getAppointmentPatientId())){
+                        patientHNId.add(a.getAppointmentPatientId());
+                    }    
+                }
             }
-            if(appointmentTimeEnd != null && !Objects.equals(a.getAppiontmentTimeEnd(), appointmentTimeEnd)){
-                a.setAppiontmentTimeEnd(appointmentTimeEnd);
-                //add sth to list                
-            }
-            if(appointmentDate != null && !Objects.equals(a.getAppointmentDate(), appointmentDate)){
-                a.setAppointmentDate(appointmentDate);
-                //add sth to list                
-            }
-            if(appointmentLocation != null && appointmentLocation.length() > 0 && !Objects.equals(a.getAppiontmentLocation(), appointmentLocation)){
-                a.setAppiontmentLocation(appointmentLocation);
-                //add sth to list  
-            }
-            if(appointmentDoctorId != 0 && !Objects.equals(a.getAppointmentDoctorId(), appointmentDoctorId)){
-                a.setAppointmentDoctorId(appointmentDoctorId);
-                //add sth to list  
-            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        //return list of appointment or list of patientId that have update (for do notification)
+        return patientHNId;
         
     }
 }
