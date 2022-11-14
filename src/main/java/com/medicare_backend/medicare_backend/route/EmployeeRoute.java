fa81ae87e3a5d90930ae3b7315e8bda3997c4c6d
Notdakon Chantarat <69079640+NtdChnn@users.noreply.github.com>
2022-11-14 13:16:45 +0700

    Add authen in Appointment and Schedule

--- src/main/java/com/medicare_backend/medicare_backend/route/EmployeeRoute.java
+++ src/main/java/com/medicare_backend/medicare_backend/route/EmployeeRoute.java
@@ -1,12 +1,6 @@
 package com.medicare_backend.medicare_backend.route;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.gson.Gson;
@@ -24,7 +18,6 @@ import com.medicare_backend.medicare_backend.service.InternalPayload;
 import com.medicare_backend.medicare_backend.service.PatientService;
 import com.medicare_backend.medicare_backend.service.PatientService;
 
-import java.lang.StackWalker.Option;
 import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.List;
